8307feffc7b64f27c1ea007d0d8e981deaeb554e
Pathum Mudannayake <instancealk@gmail.com>
2025-07-09 17:52:31 -0400

    Add flight path data for Manhattan visualization

---
+++ res/flightPathData.js
@@ -0,0 +1,10 @@
+// A sample set of points for a flight path over Manhattan, New York City.
+// Each point is represented as an array with [longitude, latitude, height in meters, time in seconds].
+// Can be replaced with similarly structured actual flight data.
+// Export the flight path array with [lon, lat, height, timeInSeconds]
+export const flightPath = [
+  [-74.0125, 40.7032, 300, 1],   // Lower Manhattan at 1 second
+  [-73.9851, 40.7580, 600, 5],   // Times Square at 5 seconds
+  [-73.9680, 40.7851, 900, 11],  // Central Park at 11 seconds
+  [-73.9496, 40.7968, 1200, 20]  // Harlem at 20 seconds
+];
