95a396b67c24895a1c350dfaf3edb60cfd76d9f5
picmate <pickmate@gmail.com>
2025-07-09 21:03:22 -0400

    Refactor comment for viewer initialization to clarify terrain source

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import { animateFlight } from './flightAnimation.js';
 Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmODg1NDQ4NS02Mzg4LTQ2ZjgtOGUxNy05Yjg5OWI1OGRjNjciLCJpZCI6ODIwNzIsImlhdCI6MTc1MTIzNTUxMX0.nb_x6vP8r1vOuCt0zo4yDw_p9SVxJk9zhUw9PwEJViM";   // <-- paste token
 
 // ------------------------------------------------------------------
-// Viewer with global curated data (World Terrain)
+// Viewer with World Terrain
 // ------------------------------------------------------------------
 const viewer = new Cesium.Viewer("cesiumContainer", {
   terrain: Cesium.Terrain.fromWorldTerrain(),
